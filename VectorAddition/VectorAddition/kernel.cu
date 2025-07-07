c380a669f98bebbfefe0417479f218de0e2bea29
thommakoon <thammakun2005@gmail.com>
2025-07-07 13:01:55 +0700

    I don't know what am I doing

---
+++ VectorAddition/VectorAddition/kernel.cu
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+__global__ void vectorAdd(const float* A, const float* B, float* C, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if(i < N) {
+        C[i] = A[i] + B[i];
+    }
+}
+
+int main() {
+    int N = 1000;
+    size_t size = N * sizeof(float);
+
+    float *h_A = new float[N];
+    float *h_B = new float[N];
+    float *h_C = new float[N];
+
+    // Initialize vectors
+    for (int i = 0; i < N; i++) {
+        h_A[i] = i;
+        h_B[i] = i * 2;
+    }
+
+    float *d_A, *d_B, *d_C;
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
+
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    // Check results
+    for(int i = 0; i < 20; i++) {
+        std::cout << h_A[i] << " + " << h_B[i] << " = " << h_C[i] << "\n";
+    }
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    delete[] h_A;
+    delete[] h_B;
+    delete[] h_C;
+
+    return 0;
+}
